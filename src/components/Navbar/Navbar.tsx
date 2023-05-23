4f2ebab9f6d770cf59c8684127a913caa7bcbd14
Thomas Bouldin <tbouldinc@gmail.com>
2023-05-23 14:33:21 -0400

    Navbar

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import "./Navbar.css";
+
+export default function Navbar() {
+  return (
+    <header>
+      <nav>
+        <a>Home</a>
+        <a>Skills</a>
+        <a>Projects</a>
+        <a className="contact">Contact</a>
+      </nav>
+    </header>
+  );
+}
