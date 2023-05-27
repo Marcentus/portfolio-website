8c6721178a0bf9ee7b4db13ef257d715268bdb56
Marcentus <tbouldinc@gmail.com>
2023-05-27 14:05:34 -0400

    Simplified

--- src/components/Main/Home/Home.tsx
+++ src/components/Main/Home/Home.tsx
@@ -1,10 +1,27 @@
 import "./Home.css";
 
 export default function Home() {
+  const skills = [
+    { img: "React-icon.svg.png", name: "REACT" },
+    { img: "TypeScript-logo.png", name: "TypeScript" },
+    { img: "JavaScript-logo.png", name: "JavaScript" },
+    { img: "html-logo.png", name: "HTML" },
+    { img: "css-logo.png", name: "CSS" },
+  ];
+
+  const skillElements = skills.map((skill) => {
+    return (
+      <li key={skill.name}>
+        <img className="skill-icon" src={`./src/assets/${skill.img}`} />
+      </li>
+    );
+  });
+
   return (
     <section className="page" id="home">
       <h1 className="my-name">Thomas Bouldin</h1>
       <h2 className="job-title">Frontend Developer</h2>
+      <ul className="home-skill-icons">{skillElements}</ul>
     </section>
   );
 }
--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,15 +1,13 @@
-import Contact from "./Contact/Contact";
 import Home from "./Home/Home";
 import Projects from "./Projects/Projects";
-import Skills from "./Skills/Skills";
+// import Skills from "./Skills/Skills";
+// import Contact from "./Contact/Contact";
 
 export default function Main() {
   return (
     <div className="main">
       <Home />
-      <Skills />
       <Projects />
-      <Contact />
     </div>
   );
 }
